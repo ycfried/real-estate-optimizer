f0a4b360dc20bff0e454ae422f31ed514c2dcac6
ycfried <ycfried1@gmail.com>
2026-05-11 02:16:15 +0000

    Fix regex

--- artifacts/mfre-tool/src/App.tsx
+++ artifacts/mfre-tool/src/App.tsx
@@ -21,7 +21,7 @@ function App() {
 return (
 <QueryClientProvider client={queryClient}>
 <TooltipProvider>
-<WouterRouter base={import.meta.env.BASE_URL.replace(//$/, "")}>
+<WouterRouter base={import.meta.env.BASE_URL.replace(/\/$/, "")}>
 <Router />
 </WouterRouter>
 <Toaster />
